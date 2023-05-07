86e92de55a4a7999a67f504039bbc6a8ad156f7c
Colin Millar <colin.millar@ices.dk>
2023-05-07 12:35:25 +0200

    cleaning up

---
+++ boot/msfd.R
@@ -0,0 +1,57 @@
+library(sf)
+library(dplyr)
+library(rmapshaper)
+# source from analysis root
+source(taf.boot.path("../utilities.R"))
+
+library(icesTAF)
+
+download.file(
+  "https://sdi.eea.europa.eu/datashare/s/EjRWzWtxxAfGWGo/download?path=%2FGPKG&files=Regional_seas_around_Europe.gpkg&downloadStartSecret=bfcr6gbo4nf",
+  destfile = "Regional_seas_around_Europe.gpkg",
+  mode = "wb"
+)
+
+# read in data
+msfd <- read_sf("Regional_seas_around_Europe.gpkg")
+
+# filter only subregions
+msfd <-
+  st_zm(msfd)  %>%
+  filter(
+    !Id %in% c("MED", "ICE", "NOR", "WHI", "BLA", "ATL", "BAR")
+  )
+
+# simplify for quick plotting
+msfd <-
+  ms_simplify(
+    msfd,
+    keep = 0.001,
+    keep_shapes = FALSE
+  )
+
+# transform to 4326
+msfd <- st_transform(msfd, 4326)
+
+# add midpoints for circles if we want them
+msfd_midpoint <- sf::st_coordinates(sf::st_centroid(msfd))
+
+msfd$LON <- msfd_midpoint[,"X"]
+msfd$LAT <- msfd_midpoint[,"Y"]
+
+# save out
+#st_write(msfd, "msfd.gpkg", append=FALSE)
+st_write(msfd, "msfd.shp", append=FALSE)
+
+# clean up
+unlink("Regional_seas_around_Europe.gpkg")
+
+if (FALSE) {
+  # some useful bits
+  msfd_data <- as.data.frame(msfd) %>% select(Id, name, spZoneType)
+  sf::st_centroid(sf::st_union(msfd))
+  plot_map(msfd,
+    strokecolor = "#097FB3",
+    fillcolor = "#AED3E4"
+  )
+}
--- utilities.R
+++ utilities.R
@@ -1,3 +1,5 @@
+
+library(leaflet)
 #helper function for choropleth animation
 setShapeStyle <- function( map, data = getMapData(map), layerId,
                            stroke = NULL, color = NULL,
@@ -6,7 +8,7 @@ setShapeStyle <- function( map, data = getMapData(map), layerId,
                            fillOpacity = NULL, dashArray = NULL,
                            smoothFactor = NULL, noClip = NULL, label = NULL,
                            options = NULL){
-  
+
   options <- c(list(layerId = layerId),
                options,
                filterNULL(list(stroke = stroke, color = color,
@@ -15,10 +17,10 @@ setShapeStyle <- function( map, data = getMapData(map), layerId,
                                fillOpacity = fillOpacity, dashArray = dashArray,
                                smoothFactor = smoothFactor, noClip = noClip, label = label
                )))
-  
+
   options <- evalFormula(options, data = data)
   options <- do.call(data.frame, c(options, list(stringsAsFactors=FALSE)))
-  
+
   layerId <- options[[1]]
   style <- options[-1]
   if("label" %in% colnames(style)){
@@ -29,48 +31,62 @@ setShapeStyle <- function( map, data = getMapData(map), layerId,
   leaflet::invokeMethod(map, data, "setStyle", "shape", layerId, style);
 }
 
-#helper function in JS for choropleth animation
-leafletjs <-  tags$head(
-  tags$script(HTML('
-  
-window.LeafletWidget.methods.setStyle = function(category, layerId, style){
-  var map = this;
-  if (!layerId){
-    return;
-  } else if (!(typeof(layerId) === "object" && layerId.length)){
-    layerId = [layerId];
-  }
-  style = HTMLWidgets.dataframeToD3(style);
-  layerId.forEach(function(d,i){
-    var layer = map.layerManager.getLayer(category, d);
-    if (layer){
-      layer.setStyle(style[i]);
-    }
-  });
-};
-window.LeafletWidget.methods.setLabel = function(category, layerId, label){
-  var map = this;
-  if (!layerId){
-    return;
-  } else if (!(typeof(layerId) === "object" && layerId.length)){
-    layerId = [layerId];
-  }
-  layerId.forEach(function(d,i){
-    var layer = map.layerManager.getLayer(category, d);
-    if (layer){
-      layer.unbindTooltip();
-      layer.bindTooltip(label[i])
-    }
-  });
-};
-'
-  ))
-)
-
-
 cumsumNA <- function(x) {
   x <- cumsum(replace(x, is.na(x), 0))
   x <- cumsum(x)
   x[x == 0] <- NA
   x
+}
+
+library(sf)
+library(ggplot2)
+plot_map <- function(data, diff_to = NULL, strokecolor = NA, fillcolor = c('#097FB3', '#A13675'),
+                     alpha = 1, graticules = FALSE, zoom_to = NULL, zoom_level = NULL) {
+  target_crs <- st_crs(data)
+
+  if (!is.null(zoom_level)) {
+    if (is.null(zoom_to)) {
+      zoom_to_xy <- st_sfc(st_point(c(0, 0)), crs = target_crs)
+    } else {
+      zoom_to_xy <- st_transform(st_sfc(st_point(zoom_to), crs = 4326), crs = target_crs)
+    }
+
+    C <- 40075016.686   # ~ circumference of Earth in meters
+    x_span <- C / 2^zoom_level
+    y_span <- C / 2^(zoom_level+1)
+
+    disp_window <- st_sfc(
+      st_point(st_coordinates(zoom_to_xy - c(x_span / 2, y_span / 2))),
+      st_point(st_coordinates(zoom_to_xy + c(x_span / 2, y_span / 2))),
+      crs = target_crs
+    )
+
+    xlim <- st_coordinates(disp_window)[,'X']
+    ylim <- st_coordinates(disp_window)[,'Y']
+  } else {
+    xlim <- NULL
+    ylim <- NULL
+  }
+
+  if (!is.null(diff_to)) {
+    shapebase <- diff_to
+    shapediff <- st_sym_difference(data, diff_to)
+  } else {
+    shapebase <- data
+    shapediff <- NULL
+  }
+
+  p <- ggplot() +
+    geom_sf(data = shapebase, color = strokecolor, fill = fillcolor[1], alpha = alpha)
+
+  if (!is.null(shapediff)) {
+    p <- p + geom_sf(data = shapediff, color = strokecolor, fill = fillcolor[2], alpha = alpha)
+  }
+
+  p +
+    coord_sf(xlim = xlim,
+             ylim = ylim,
+             crs = target_crs,
+             datum = ifelse(graticules, target_crs$input, NA)) +
+    theme_bw()
 }
